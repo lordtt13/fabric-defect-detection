c7295bc321966de204edc1618f5cf6914ea6625e
Tanmay Thakur <thakurtanmay72@yahoo.com>
2019-12-24 02:17:46 +0530

    Model Lessened

--- train_.py
+++ train_.py
@@ -5,6 +5,12 @@ Created on Sat Dec 21 22:15:13 2019
 @author: tanma
 """
 
+import tensorflow as tf
+from keras.backend.tensorflow_backend import set_session
+config = tf.ConfigProto()
+config.gpu_options.per_process_gpu_memory_fraction = 0.6
+set_session(tf.Session(config=config))
+
 import matplotlib.pyplot as plt
 from utils import train_val_generator
 from keras.layers import Conv2D,MaxPool2D,Dense,Dropout,Flatten,Input
@@ -16,8 +22,8 @@ from keras.optimizers import Adam
 train_path = './train'
 test_path ='./test'
 
-train_gen, val_gen = train_val_generator(32,train_path,test_path)
-input_shape = (224,224,3)
+train_gen, val_gen = train_val_generator(16,train_path,test_path)
+input_shape = (128,128,3)
 
 X_in = Input(input_shape)
 X = Conv2D(64,3,activation='relu',padding='same')(X_in)
@@ -25,7 +31,7 @@ X = MaxPool2D(pool_size=(2,2))(X)
 X = Conv2D(128,3,activation='relu',padding='same')(X)
 X = MaxPool2D(pool_size=(2,2))(X)
 X = Flatten()(X)
-X = Dense(4096,activation='relu')(X)
+X = Dense(256,activation='relu')(X)
 X_out = Dense(2,activation='softmax')(X)
 tr_model = Model(input = X_in, output = X_out)
 
@@ -37,7 +43,7 @@ tr_model.compile(loss='categorical_crossentropy',optimizer=Adam(1e-5),metrics=['
 # train the model
 history = tr_model.fit_generator(
                 train_gen,
-                steps_per_epoch=1400,
+                steps_per_epoch=1000,
                 epochs=30,
                 validation_data = val_gen,
                 validation_steps = 250,
